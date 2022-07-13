9121b737b8bbfe0c2288869ab7f84ae759649141
liangjingjie <liangjingjie@52tt.com>
2022-07-13 20:48:23 +0800

    添加注释

--- app/src/main/java/com/quwan/cameraxdemo/CameraActivity.kt
+++ app/src/main/java/com/quwan/cameraxdemo/CameraActivity.kt
@@ -2,8 +2,10 @@ package com.quwan.cameraxdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.util.Log
 import android.view.View
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
@@ -18,6 +20,7 @@ class CameraActivity : AppCompatActivity() {
     private var cameraProvider: ProcessCameraProvider? = null
     private var previewUseCase: Preview? = null
     private lateinit var previewView: PreviewView
+    private lateinit var tvFps: AppCompatTextView
     private var cameraSelector: CameraSelector? = null
     private var lensFacing = CameraSelector.LENS_FACING_FRONT
     private var camera: Camera? = null
@@ -27,6 +30,7 @@ class CameraActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
         previewView = findViewById(R.id.preview_view)
+        tvFps = findViewById(R.id.tvFps)
         findViewById<View>(R.id.switchCamera).setOnClickListener {
             switchCamera()
         }
@@ -40,6 +44,10 @@ class CameraActivity : AppCompatActivity() {
     }
 
     private fun start() {
+        /*
+        通过 ProcessCameraProvider 获取 cameraProvider
+        cameraProvider 就是我们持有的相机实例
+        */
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
         cameraProviderFuture.addListener(Runnable {
             cameraProvider = cameraProviderFuture.get()
@@ -58,14 +66,25 @@ class CameraActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     *      启动相机
+     * */
     private fun startCamera() {
         if (cameraProvider != null) {
             cameraProvider!!.unbindAll()
+            /*
+            这一步是绑定预览界面，如果不需要预览界面，这一步克注释掉
+            CameraX优势体验之一：预览界面可以根据开发者需求去取舍，而Camera1和Camera2则必须要预览界面
+            */
             bindPreviewUseCase()
+            // 这一步是绑定相机预览数据，可以获得相机每一帧的数据
             bindAnalysisUseCase()
         }
     }
 
+    /**
+     * 绑定预览界面。不需要预览界面可以不调用
+     * */
     private fun bindPreviewUseCase() {
         if (previewUseCase != null) {
             cameraProvider!!.unbind(previewUseCase)
@@ -82,6 +101,13 @@ class CameraActivity : AppCompatActivity() {
         )
     }
 
+    private var lastTime = 0L
+    private var lastShowTime = 0L
+    private var count = 0
+    private var maxFrameMs = 0L
+    private var minFrameMs = Long.MAX_VALUE
+
+
     private fun bindAnalysisUseCase() {
         if (analysisUseCase != null) {
             cameraProvider!!.unbind(analysisUseCase)
@@ -91,7 +117,22 @@ class CameraActivity : AppCompatActivity() {
         analysisUseCase?.setAnalyzer(
             ContextCompat.getMainExecutor(this)
         ) { imageProxy: ImageProxy ->
+            count += 1
+            val currentTime = SystemClock.elapsedRealtime()
+            val d = currentTime - lastShowTime
+            maxFrameMs = maxFrameMs.coerceAtLeast(d)
+            minFrameMs = minFrameMs.coerceAtMost(d)
+            if ((currentTime - lastTime) >= 1000){
+                tvFps.text = "fps: ${count}, 两帧最大：${maxFrameMs}, 最小：${minFrameMs}"
+                lastTime = currentTime
+                count = 0
+                maxFrameMs = 0
+                minFrameMs = Long.MAX_VALUE
+            }
+            lastShowTime = currentTime
             Log.d(TAG,"ImageProxy: 宽高： ${imageProxy.width} * ${imageProxy.height}")
+
+            //必须close,相机才会下发下一帧数据,否则会一直阻塞相机下发数据
             imageProxy.close()
         }
         camera = cameraProvider!!.bindToLifecycle(
@@ -101,7 +142,7 @@ class CameraActivity : AppCompatActivity() {
         )
     }
 
-    fun switchCamera() {
+    private fun switchCamera() {
         if (cameraProvider == null) {
             return
         }
